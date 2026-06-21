8cbe6ea1afa15d895aa8cea4d4e72c1604a168d5
Nitin Kumar <177527946+Nitin225@users.noreply.github.com>
2026-06-21 17:14:18 +0530

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> graph(n);
+        for(auto d : edges){
+            graph[d[1]].push_back(d[0]);
+        }
+        
+        vector<int> indegree(n,0);
+        for(int i=0; i<n; i++){
+            for(auto it : graph[i]){
+                indegree[it]++;
+            }
+        }
+    
+        queue<int> q;
+        for(int i=0; i<n; i++){
+            if(!indegree[i]){
+                q.push(i);
+            }
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto it : graph[node]){
+                indegree[it]--;
+                if(indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+        if(ans.size() != n) return {};
+        return ans;
+        
+
+    }
+};
