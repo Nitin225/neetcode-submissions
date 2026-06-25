d7c613af9b5d29320a2f58a92f2082c4b630345f
Nitin Kumar <177527946+Nitin225@users.noreply.github.com>
2026-06-26 02:24:48 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.cpp
@@ -0,0 +1,56 @@
+class DisjointSet {
+public:
+    vector<int> parent, size;
+
+    DisjointSet(int n) {
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+
+        for(int i = 0; i <= n; i++)
+            parent[i] = i;
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node;
+
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v) {
+        int pu = findUPar(u);
+        int pv = findUPar(v);
+
+        if(pu == pv) return;
+
+        if(size[pu] < size[pv]) {
+            parent[pu] = pv;
+            size[pv] += size[pu];
+        }
+        else {
+            parent[pv] = pu;
+            size[pu] += size[pv];
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+
+        int m = edges.size();
+        DisjointSet ds(m);
+
+        for(auto &e : edges) {
+            int u = e[0];
+            int v = e[1];
+
+            if(ds.findUPar(u) == ds.findUPar(v))
+                return {u, v};
+
+            ds.unionBySize(u, v);
+        }
+
+        return {};
+    }
+};
