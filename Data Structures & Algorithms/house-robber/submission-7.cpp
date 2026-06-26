56be18a0b8d47b63a310f6118c07639d537a1e52
Nitin Kumar <177527946+Nitin225@users.noreply.github.com>
2026-06-26 14:41:59 +0530

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size()-1;
+        if(n == 0) return nums[0];
+
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
+
+        for(int i=2; i<n+1; i++){
+            int curr = max(nums[i] + prev2, prev1);
+            
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
