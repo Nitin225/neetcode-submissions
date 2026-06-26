6158ca208995fafc3ff60d159ed33c40bc889c9d
Nitin Kumar <177527946+Nitin225@users.noreply.github.com>
2026-06-26 14:20:50 +0530

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private:
+    int f(int n, vector<int> &nums, vector<int> &dp){
+        if(n == 0) return nums[0];
+        if(n < 0) return 0;
+
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = max(nums[n] + f(n-2, nums, dp), f(n-1, nums, dp));
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return f(n-1, nums, dp);
+    }
+};
