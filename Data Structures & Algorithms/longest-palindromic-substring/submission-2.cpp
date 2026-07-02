bd57886ec5e350923f9e59bff67556f465851904
Nitin Kumar <177527946+Nitin225@users.noreply.github.com>
2026-07-02 10:15:18 +0530

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        int st = 0, maxLen = 1;
+
+        for (int i = 0; i < n; i++)
+            dp[i][i] = true;
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if (s[i] == s[j]) {
+                    if (len == 2 || dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+
+                        if (len > maxLen) {
+                            maxLen = len;
+                            st = i;
+                        }
+                    }
+                }
+            }
+        }
+        return s.substr(st, maxLen);
+    }
+};
