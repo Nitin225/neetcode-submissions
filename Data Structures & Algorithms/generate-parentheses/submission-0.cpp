8bf22ccccef1624a52c8ce424b482ba1395fec05
Nitin Kumar <177527946+Nitin225@users.noreply.github.com>
2026-07-10 13:41:58 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    void helper(int i, int open, int a, int n, string s, vector<string> &ans){
+        
+        if(a < 0) return;
+
+        if(i == 2*n && a == 0){
+            ans.push_back(s);
+            return;
+        }
+        if(open < n)
+            helper(i+1, open+1, a+1, n, s+"(", ans);
+        if(a > 0)
+            helper(i+1, open, a-1, n, s+")", ans);
+
+        
+    }
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        helper(0, 0, 0, n, "", ans);
+        return ans;
+    }
+};
